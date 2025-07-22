6e42bf7e7978f37e7ab9fbc22c7dc1e9815f851d
Tetiana <t.bandrivska04@gmail.com>
2025-07-22 16:37:09 +0000

    Solved an excercise

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -3,9 +3,21 @@
 // Using Variables, by Eduardo Corpeño 
 
 #include <iostream>
-
+int a,b = 5;
 int main(){
-    
+    bool my_flag;
+    a = 7;
+    std::cout<< "a=" << a << std::endl;
+    std::cout<< "b=" << b << std::endl;
+    std::cout<< "flag=" << my_flag << std::endl;
+    my_flag = true;
+
+    std::cout<< "flag =" << my_flag << std::endl;
+    std::cout<< "a + b" << a + b << std::endl;
+    std::cout<< "a - b" << a - b << std::endl;
+
+
+
     std::cout << std::endl << std::endl;
     return 0;
 }
