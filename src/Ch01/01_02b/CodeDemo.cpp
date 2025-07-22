62459512aeecbd4fcf2ba1bcb97c5fb34602d192
Tetiana <t.bandrivska04@gmail.com>
2025-07-22 16:32:09 +0000

    Solved an excercise

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,10 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include <iostream>
+
+int main(){
+    std::cout << "Hi There" << std::endl;
+    return 0;
+    std::cout << std::endl;
+}
